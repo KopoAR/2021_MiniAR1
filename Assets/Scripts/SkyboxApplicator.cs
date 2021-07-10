63eedc6a71fb08599f2dbc489a953588cd1dc6c1
BritzSTM <30376375+BritzSTM@users.noreply.github.com>
2021-07-11 00:49:17 +0900

    Add XR interaction kit, new input system

--- Assets/Scripts/SkyboxApplicator.cs
+++ Assets/Scripts/SkyboxApplicator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace mv
@@ -26,28 +27,37 @@ namespace mv
 
         private void Awake()
         {
-            if (_namedSkyboxs.Length > 0
-                && _namedSkyboxs[_selection].Material != null)
-            {
-                ChangeSkybox();
-            }
+            ChangeSkybox(_selection);
         }
+
+        private bool IsValidSelection(int index) => (index < _namedSkyboxs.Length) && (_namedSkyboxs[index].Material != null);
 
         private void OnValidate()
         {
-            if (_selection < _namedSkyboxs.Length
-                && _namedSkyboxs[_selection].Material != null)
-            {
-                _correctSelection = _selection;
-                ChangeSkybox();
-            }
-            else
+            if (!IsValidSelection(_selection))
             {
                 _selection = _correctSelection;
                 Debug.LogWarning("invalid skybox section.");
+                return;
             }
+
+            _correctSelection = _selection;
+            ChangeSkybox(_selection);
         }
 
-        private void ChangeSkybox() => RenderSettings.skybox = _namedSkyboxs[_selection].Material;
+        public void ChangeSkybox(int index)
+        {
+            if (!IsValidSelection(index))
+                return;
+            
+            RenderSettings.skybox = _namedSkyboxs[index].Material;
+        }
+
+        public void ChangeSkybox(string name)
+        {
+            var found = _namedSkyboxs.FirstOrDefault(x => x.Name == name);
+            if(found.Material != null)
+                RenderSettings.skybox = found.Material;
+        }
     }
 }
