be50490ad289b98eed1269cfd7b7a2429962752d
BritzSTM <30376375+BritzSTM@users.noreply.github.com>
2021-07-12 15:36:12 +0900

    Create 스토리 제어를 위한 간단한 SO

---
+++ Assets/Scripts/StroySEQ.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "new stroy seq", menuName = "Game/StroySEQ")]
+public class StroySEQ : ScriptableObject
+{
+    public int ActNumber;
+    public int SubNumber;
+}
