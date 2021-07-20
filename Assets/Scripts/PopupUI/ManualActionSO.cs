ff57d66f3410aae4ce2bff2d70feac593a21dccb
nohzoe <80494430+nohzoe@users.noreply.github.com>
2021-07-20 18:07:21 +0900

    배치중
    아이템 배치 중 거리 한계 있음

--- Assets/Scripts/PopupUI/ManualActionSO.cs
+++ Assets/Scripts/PopupUI/ManualActionSO.cs
@@ -6,13 +6,14 @@ using UnityEngine.Events;
 [CreateAssetMenu(fileName ="new active manual", menuName ="Game/ManualActionSO")]
 public class ManualActionSO : ScriptableObject
 {
+//    public UnityAction<ManualSO, Vector3> activeAtion;
     public UnityAction<ManualSO> activeAtion;
     public UnityAction closeAction;
 
-    public void Active(ManualSO data)
+    public void Active(ManualSO data)//, Vector3 pos)
     {
         if (activeAtion != null)
-            activeAtion(data);
+            activeAtion(data); //, pos);
     }
 
     public void CloseActive()
