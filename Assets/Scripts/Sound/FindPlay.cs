aa58a028e02bde7ba95ea7dc416fd69e643255b0
c-vely <84121069+c-vely@users.noreply.github.com>
2021-07-26 18:25:43 +0900

    fixed sound & locker scene_0726 (#71)
    * fixed sound & locker scene_0726
    
    fixed sound & locker scene
    by choi
    
    * Delete Search.index
    
    * Delete Search.settings
    
    Co-authored-by: BritzSTM <30376375+BritzSTM@users.noreply.github.com>

--- Assets/Scripts/Sound/FindPlay.cs
+++ Assets/Scripts/Sound/FindPlay.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class FindPlay : MonoBehaviour
 {
     private BGMManager bgmManager;
+    public int bgmNum;
 
 
     void Start()
     {
-        bgmManager = BGMManager.instance;
-        bgmManager.Stop(9);
-        PlayBGM();
-
+        StartCoroutine(TryPlayBGM());
     }
 
-    public void PlayBGM()
+    private IEnumerator TryPlayBGM()
     {
-        bgmManager.Play(7);
-        bgmManager.SetVoulme(0.2f, 7);
+        while (BGMManager.instance == null)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+
+        bgmManager = BGMManager.instance;
+        PlayBGM(bgmNum);
+    }
+
+    public void PlayBGM(int n)
+    {
+        bgmManager.Play(n);
+        bgmManager.SetVoulme(0.2f, n);
     }
 }
